48ae3c2ce5078e25b053219d50a48b3ed2fdb55c
AveryOn <lnkonion2@gmail.com>
2025-08-26 18:20:46 +0400

    fix

--- setup.sh
+++ setup.sh
@@ -15,7 +15,21 @@ if ! command -v curl &> /dev/null; then
 fi
 
 # Docker и docker compose
-apt install -y docker.io docker-compose-plugin
+# Установка Docker CE и compose plugin
+apt update
+apt install -y ca-certificates curl gnupg lsb-release
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 # Установка NVM + Node.js
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
